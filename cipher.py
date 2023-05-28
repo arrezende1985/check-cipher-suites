fdba2c461423b009a6c3706f909cc135e25782e7
Alexandre Rezende <alexandre.rrezende@gmail.com>
2023-05-27 22:00:06 -0300

    Translate from PT-BR to EN-US

--- cipher.py
+++ cipher.py
@@ -14,82 +14,108 @@ from collections import defaultdict
 from ipwhois import IPWhois
 
 
-# Cores ANSI
-class Cores:
-    VERMELHO = '\033[91m'
-    VERDE = '\033[92m'
-    AMARELO = '\033[93m'
-    AZUL = '\033[94m'
+# ANSI Colors
+class Colors:
+    RED = '\033[91m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    BLUE = '\033[94m'
     MAGENTA = '\033[95m'
     RESET = '\033[0m'
 
 
-def verifica_ciphersuite_protocolo(host, porta):
-    # Cria um contexto SSL/TLS personalizado
-    contexto = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+def check_ciphersuite_protocol(host, port):
+    # Create a custom SSL/TLS context
+    context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
 
-    # Desativa a verificação do certificado
-    contexto.check_hostname = False
-    contexto.verify_mode = ssl.CERT_NONE
+    # Disable certificate verification
+    context.check_hostname = False
+    context.verify_mode = ssl.CERT_NONE
 
-    # Obtém todas as ciphersuites suportadas pelo contexto
-    ciphersuites = contexto.get_ciphers()
+    # Get all supported ciphersuites by the context
+    ciphersuites = context.get_ciphers()
 
-    # Agrupa as ciphersuites por versão do TLS
-    ciphersuites_por_versao = defaultdict(list)
+    # Group ciphersuites by TLS version
+    ciphersuites_by_version = defaultdict(list)
     for ciphersuite in ciphersuites:
-        versao_tls = ciphersuite['protocol']
-        ciphersuites_por_versao[versao_tls].append(ciphersuite)
+        tls_version = ciphersuite['protocol']
+        ciphersuites_by_version[tls_version].append(ciphersuite)
 
-    # Exibe as ciphersuites por versão do TLS
-    print(f"{Cores.AZUL}Ciphersuites compatíveis:{Cores.RESET}")
-    for versao_tls, ciphersuites in ciphersuites_por_versao.items():
-        print(f"{Cores.AMARELO}Versão do protocolo TLS: {versao_tls}{Cores.RESET}")
+    # Display ciphersuites by TLS version
+    print(f"\n{Colors.BLUE}Compatible ciphersuites:{Colors.RESET}")
+    for tls_version, ciphersuites in ciphersuites_by_version.items():
+        print(f"{Colors.YELLOW}TLS protocol version: {tls_version}{Colors.RESET}")
         for ciphersuite in ciphersuites:
-            print(f"  {Cores.VERDE}Algoritmo:{Cores.RESET} {ciphersuite['name']}")
+            print(f"  {Colors.GREEN}Algorithm:{Colors.RESET} {ciphersuite['name']}")
             if 'keySize' in ciphersuite:
-                print(f"  {Cores.VERDE}Bits da chave:{Cores.RESET} {ciphersuite['keySize']}")
+                print(f"    {Colors.GREEN}Key size:{Colors.RESET} {ciphersuite['keySize']}")
         print()
 
-def obter_enderecos_ip(host):
-    # Obtém uma lista de endereços IP do host
-    enderecos_ip = socket.gethostbyname_ex(host)[2]
 
-    # Verifica se há endereços IP disponíveis
-    if len(enderecos_ip) > 0:
-        print(f"{Cores.AMARELO}Endereços IP:{Cores.RESET}")
-        for ip in enderecos_ip:
-            print(f"  {Cores.AMARELO}Endereço IP:{Cores.RESET} {ip}")
+def get_ip_addresses(host):
+    # Get a list of IP addresses for the host
+    ip_addresses = socket.gethostbyname_ex(host)[2]
 
-            # Verifica o tipo de IP (Internet ou Rede Interna)
+    # Check if there are available IP addresses
+    if len(ip_addresses) > 0:
+        print(f"\n{Colors.YELLOW}IP Addresses:{Colors.RESET}")
+        for ip in ip_addresses:
+            print(f"  {Colors.YELLOW}IP Address:{Colors.RESET} {ip}")
+
+            # Get domain information
+            try:
+                domain_info = socket.gethostbyaddr(ip)
+                domain_name = domain_info[0]
+                print(f"    {Colors.YELLOW}Domain:{Colors.RESET} {domain_name}")
+                if len(domain_info[1]) > 0:
+                    print(f"    {Colors.YELLOW}Hostname aliases:{Colors.RESET}")
+                    for alias in domain_info[1]:
+                        print(f"      {alias}")
+                if len(domain_info[2]) > 0:
+                    print(f"    {Colors.YELLOW}IP addresses associated with the domain:{Colors.RESET}")
+                    for ip_alias in domain_info[2]:
+                        print(f"      {ip_alias}")
+            except socket.herror:
+                print(f"    {Colors.YELLOW}Domain:{Colors.RESET} Not found")
+
+            # Check the type of IP (Internet or Internal Network)
             obj = IPWhois(ip)
             info = obj.lookup_rdap()
             if info['asn_country_code']:
-                print(f"    {Cores.AMARELO}Tipo de IP:{Cores.RESET} Internet")
-                print(f"    {Cores.AMARELO}Localização:{Cores.RESET} {info['asn_country_code']} - {info['asn_description']}")
+                print(f"    {Colors.YELLOW}IP Type:{Colors.RESET} Internet")
+                print(f"    {Colors.YELLOW}Location:{Colors.RESET} {info['asn_country_code']} - {info['asn_description']}")
+                print(f"    {Colors.YELLOW}ASN:{Colors.RESET} {info['asn']}")
+                print(f"    {Colors.YELLOW}CIDR:{Colors.RESET} {info['network']['cidr']}")
             else:
-                print(f"    {Cores.AMARELO}Tipo de IP:{Cores.RESET} Rede Interna")
+                print(f"    {Colors.YELLOW}IP Type:{Colors.RESET} Internal Network")
             print()
     else:
-        print(f"{Cores.AMARELO}Nenhum endereço IP encontrado para o host {host}{Cores.RESET}")
+        print(f"{Colors.YELLOW}No IP addresses found for the host {host}{Colors.RESET}")
 
 
-# Limpa o terminal
-def limpar_terminal():
-    if os.name == 'posix':  # Sistema tipo Unix
+# Clear the terminal
+def clear_terminal():
+    if os.name == 'posix':  # Unix-like system
         os.system('clear')
-    elif os.name == 'nt':  # Sistema Windows
+    elif os.name == 'nt':  # Windows system
         os.system('cls')
 
 
 def main():
-    limpar_terminal()
-    host = input("Digite o host: ")
-    porta = int(input("Digite a porta: "))
+    clear_terminal()
+    host = input("Enter the host: ")
+    port = int(input("Enter the port: "))
 
-    limpar_terminal()
-    obter_enderecos_ip(host)
-    verifica_ciphersuite_protocolo(host, porta)
+    clear_terminal()
+    print("Consulting information. Please wait...")
+
+    clear_terminal()
+    get_ip_addresses(host)
+    check_ciphersuite_protocol(host, port)
+
+    # Limpar variáveis
+    host = None
+    port = None
 
 
 if __name__ == '__main__':
